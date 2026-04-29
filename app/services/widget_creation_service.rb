a9f84f4a7d226291ef11ba170108951257ded288
Shivam Mishra <scm.mymail@gmail.com>
2026-04-29 14:27:07 +0530

    feat: add WidgetCreationService for onboarding web widget setup
    Builds a Channel::WebWidget, Inbox and InboxMember in one transaction,
    seeded from the account's name, domain and brand_info enrichment data
    (widget color, welcome title, tagline). Includes a prepend_mod_with hook
    so the Enterprise edition can override tagline generation.

---
+++ app/services/widget_creation_service.rb
@@ -0,0 +1,54 @@
+class WidgetCreationService
+  DEFAULT_WIDGET_COLOR = '#1f93ff'.freeze
+
+  def initialize(account, user)
+    @account = account
+    @user = user
+  end
+
+  def perform
+    ActiveRecord::Base.transaction do
+      channel = build_channel
+      inbox = @account.inboxes.create!(name: @account.name, channel: channel)
+      InboxMember.find_or_create_by!(inbox: inbox, user: @user)
+      inbox
+    end
+  rescue StandardError => e
+    Rails.logger.error "[WidgetCreation] #{e.message}"
+    nil
+  end
+
+  private
+
+  def build_channel
+    @account.web_widgets.create!(
+      website_url: website_url,
+      widget_color: widget_color,
+      welcome_title: welcome_title,
+      welcome_tagline: welcome_tagline_text
+    )
+  end
+
+  def brand_info
+    @brand_info ||= (@account.custom_attributes['brand_info'] || {}).deep_symbolize_keys
+  end
+
+  def website_url
+    @account.domain.presence || brand_info[:domain].to_s
+  end
+
+  def widget_color
+    hex = brand_info[:colors]&.first&.dig(:hex)
+    hex.to_s.match?(/\A#\h{6}\z/) ? hex : DEFAULT_WIDGET_COLOR
+  end
+
+  def welcome_title
+    brand_info[:title].presence || @account.name
+  end
+
+  def welcome_tagline_text
+    brand_info[:slogan].presence || brand_info[:description].presence
+  end
+end
+
+WidgetCreationService.prepend_mod_with('WidgetCreationService')
