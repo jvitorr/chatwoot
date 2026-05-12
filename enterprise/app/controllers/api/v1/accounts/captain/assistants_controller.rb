cognis-ai/cognis-support

fix(captain): handle resolved conversation context

enterprise/app/controllers/api/v1/accounts/captain/assistants_controller.rb
@@ -59,7 +59,7 @@ class Api::V1::Accounts::Captain::AssistantsController < Api::V1::Accounts::Base
                                                     :product_name, :feature_faq, :feature_memory, :feature_citation,
                                                     :feature_contact_attributes,
                                                     :welcome_message, :handoff_message, :resolution_message,
-                                                    :instructions, :temperature
+                                                    :instructions, :temperature, :resolved_context_handling
                                                   ])
 
     # Handle array parameters separately to allow partial updates

enterprise/app/services/captain/conversation/message_history_builder_service.rb
@@ -1,0 +1,52 @@
+class Captain::Conversation::MessageHistoryBuilderService
+  pattr_initialize [:conversation!, :assistant!]
+
+  def perform
+    conversation_messages_for_context.filter_map do |message|
+      message_hash = message_hash_for_context(message)
+      next if message_hash.blank?
+
+      message_hash[:agent_name] = message.additional_attributes['agent_name'] if message.additional_attributes&.dig('agent_name').present?
+      message_hash
+    end
+  end
+
+  private
+
+  def conversation_messages_for_context
+    messages = conversation.messages.where(private: false)
+    messages = messages.where('created_at > ?', conversation.last_resolved_at) if strict_resolved_context_boundary?
+    messages.where(message_type: context_message_types)
+  end
+
+  def strict_resolved_context_boundary?
+    assistant.resolved_context_strict_reset? && conversation.last_resolved_at.present?
+  end
+
+  def context_message_types
+    return [:incoming, :outgoing, :activity] if assistant.resolved_context_activity_marker?
+
+    [:incoming, :outgoing]
+  end
+
+  def message_hash_for_context(message)
+    return activity_message_hash(message) if message.message_type == 'activity'
+
+    {
+      content: prepare_multimodal_message_content(message),
+      role: determine_role(message)
+    }
+  end
+
+  def activity_message_hash(message)
+    Captain::ActivityMessageContextBuilderService.new(message: message).generate_content
+  end
+
+  def determine_role(message)
+    message.message_type == 'incoming' ? 'user' : 'assistant'
+  end
+
+  def prepare_multimodal_message_content(message)
+    Captain::OpenAiMessageBuilderService.new(message: message).generate_content
+  end
+end

spec/enterprise/jobs/captain/conversation/response_builder_job_spec.rb
@@ -45,6 +45,63 @@ RSpec.describe Captain::Conversation::ResponseBuilderJob, type: :job do
         expect(conversation.messages.last.content).to eq('Hey, welcome to Captain Specs')
       end
 
+      it 'keeps the default message history limited to public chat messages' do
+        create(
+          :message,
+          conversation: conversation,
+          message_type: :activity,
+          content: 'Conversation was marked resolved',
+          content_attributes: { activity: { type: 'conversation_status_changed', status: 'resolved' } }
+        )
+        create(:message, conversation: conversation, content: 'Private note', message_type: :outgoing, private: true)
+
+        expect(mock_llm_chat_service).to receive(:generate_response).with(
+          message_history: [{ content: 'Hello', role: 'user' }]
+        ).and_return({ 'response' => 'Hey, welcome to Captain Specs' })
+
+        described_class.perform_now(conversation, assistant)
+      end
+
+      it 'includes resolved activity markers when activity marker mode is enabled' do
+        assistant.update!(config: { resolved_context_handling: 'activity_marker' })
+        create(
+          :message,
+          conversation: conversation,
+          message_type: :activity,
+          content: 'Conversation was marked resolved',
+          content_attributes: { activity: { type: 'conversation_status_changed', status: 'resolved' } }
+        )
+        create(:message, conversation: conversation, message_type: :activity, content: 'Assigned to agent')
+        create(:message, conversation: conversation, content: 'Fresh question', message_type: :incoming)
+
+        expect(mock_llm_chat_service).to receive(:generate_response) do |message_history:|
+          expect(message_history).to eq([
+                                          { content: 'Hello', role: 'user' },
+                                          {
+                                            content: Captain::ActivityMessageContextBuilderService::RESOLVED_CONTEXT,
+                                            role: 'system'
+                                          },
+                                          { content: 'Fresh question', role: 'user' }
+                                        ])
+          { 'response' => 'Hey, welcome to Captain Specs' }
+        end
+
+        described_class.perform_now(conversation, assistant)
+      end
+
+      it 'sends only messages after last_resolved_at when strict reset mode is enabled' do
+        assistant.update!(config: { resolved_context_handling: 'strict_reset' })
+        conversation.messages.incoming.last.update!(created_at: 10.minutes.ago)
+        conversation.update!(last_resolved_at: 5.minutes.ago)
+        create(:message, conversation: conversation, content: 'New issue', message_type: :incoming)
+
+        expect(mock_llm_chat_service).to receive(:generate_response).with(
+          message_history: [{ content: 'New issue', role: 'user' }]
+        ).and_return({ 'response' => 'Hey, welcome to Captain Specs' })
+
+        described_class.perform_now(conversation, assistant)
+      end
+
       it 'increments usage response' do
         described_class.perform_now(conversation, assistant)
         account.reload

jvitorr/chatwoot

enterprise/app/controllers/api/v1/accounts/captain/assistants_controller.rb
@@ -59,7 +59,7 @@ class Api::V1::Accounts::Captain::AssistantsController < Api::V1::Accounts::Base
                                                     :product_name, :feature_faq, :feature_memory, :feature_citation,
                                                     :feature_contact_attributes,
                                                     :welcome_message, :handoff_message, :resolution_message,
-                                                    :instructions, :temperature
+                                                    :instructions, :temperature, :conversation_context
                                                   ])
 
     # Handle array parameters separately to allow partial updates

enterprise/app/services/captain/conversation/message_history_builder_service.rb
@@ -1,0 +1,52 @@
+class Captain::Conversation::MessageHistoryBuilderService
+  pattr_initialize [:conversation!, :assistant!]
+
+  def perform
+    conversation_messages_for_context.filter_map do |message|
+      message_hash = message_hash_for_context(message)
+      next if message_hash.blank?
+
+      message_hash[:agent_name] = message.additional_attributes['agent_name'] if message.additional_attributes&.dig('agent_name').present?
+      message_hash
+    end
+  end
+
+  private
+
+  def conversation_messages_for_context
+    messages = conversation.messages.where(private: false)
+    messages = messages.where('created_at > ?', conversation.last_resolved_at) if since_last_resolution_boundary?
+    messages.where(message_type: context_message_types)
+  end
+
+  def since_last_resolution_boundary?
+    assistant.conversation_context_since_last_resolution? && conversation.last_resolved_at.present?
+  end
+
+  def context_message_types
+    return [:incoming, :outgoing, :activity] if assistant.conversation_context_with_resolution_markers?
+
+    [:incoming, :outgoing]
+  end
+
+  def message_hash_for_context(message)
+    return activity_message_hash(message) if message.message_type == 'activity'
+
+    {
+      content: prepare_multimodal_message_content(message),
+      role: determine_role(message)
+    }
+  end
+
+  def activity_message_hash(message)
+    Captain::ActivityMessageContextBuilderService.new(message: message).generate_content
+  end
+
+  def determine_role(message)
+    message.message_type == 'incoming' ? 'user' : 'assistant'
+  end
+
+  def prepare_multimodal_message_content(message)
+    Captain::OpenAiMessageBuilderService.new(message: message).generate_content
+  end
+end

spec/enterprise/jobs/captain/conversation/response_builder_job_spec.rb
@@ -45,6 +45,63 @@ RSpec.describe Captain::Conversation::ResponseBuilderJob, type: :job do
         expect(conversation.messages.last.content).to eq('Hey, welcome to Captain Specs')
       end
 
+      it 'keeps the default message history limited to public chat messages' do
+        create(
+          :message,
+          conversation: conversation,
+          message_type: :activity,
+          content: 'Conversation was marked resolved',
+          content_attributes: { activity: { type: 'conversation_status_changed', status: 'resolved' } }
+        )
+        create(:message, conversation: conversation, content: 'Private note', message_type: :outgoing, private: true)
+
+        expect(mock_llm_chat_service).to receive(:generate_response).with(
+          message_history: [{ content: 'Hello', role: 'user' }]
+        ).and_return({ 'response' => 'Hey, welcome to Captain Specs' })
+
+        described_class.perform_now(conversation, assistant)
+      end
+
+      it 'includes resolution markers when resolution marker mode is enabled' do
+        assistant.update!(config: { conversation_context: 'with_resolution_markers' })
+        create(
+          :message,
+          conversation: conversation,
+          message_type: :activity,
+          content: 'Conversation was marked resolved',
+          content_attributes: { activity: { type: 'conversation_status_changed', status: 'resolved' } }
+        )
+        create(:message, conversation: conversation, message_type: :activity, content: 'Assigned to agent')
+        create(:message, conversation: conversation, content: 'Fresh question', message_type: :incoming)
+
+        expect(mock_llm_chat_service).to receive(:generate_response) do |message_history:|
+          expect(message_history).to eq([
+                                          { content: 'Hello', role: 'user' },
+                                          {
+                                            content: Captain::ActivityMessageContextBuilderService::RESOLVED_CONTEXT,
+                                            role: 'system'
+                                          },
+                                          { content: 'Fresh question', role: 'user' }
+                                        ])
+          { 'response' => 'Hey, welcome to Captain Specs' }
+        end
+
+        described_class.perform_now(conversation, assistant)
+      end
+
+      it 'sends only messages after last_resolved_at when strict reset mode is enabled' do
+        assistant.update!(config: { conversation_context: 'since_last_resolution' })
+        conversation.messages.incoming.last.update!(created_at: 10.minutes.ago)
+        conversation.update!(last_resolved_at: 5.minutes.ago)
+        create(:message, conversation: conversation, content: 'New issue', message_type: :incoming)
+
+        expect(mock_llm_chat_service).to receive(:generate_response).with(
+          message_history: [{ content: 'New issue', role: 'user' }]
+        ).and_return({ 'response' => 'Hey, welcome to Captain Specs' })
+
+        described_class.perform_now(conversation, assistant)
+      end
+
       it 'increments usage response' do
         described_class.perform_now(conversation, assistant)
         account.reload